4827f9a550e113241fedff8522ec1d574fc83a92
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2014-09-29 19:51:51 +0200

    Klar med labben tror jag, har några felkoder som jag inte förstår mig på. ska diskutera med klassen imorgon

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -8,19 +8,46 @@ namespace _1DV402.S2.L1A
 {
     class SecretNumber
     {
-        int _Count;
-        int _Number;
-        public const int MaxNumberOffGuesses = 7;
-        public SecretNumber()
+        int _count; //Variabel för hur många chanser man har
+        int _number; // slumpgenereade numret
+        public const int MaxNumberOffGuesses = 7; //Hur många gissningar man ska få.
+        public SecretNumber() // Här Startas programmet och initialize
         {
-
+            Initialize();
         }
-        public void Initialize()
+        public void Initialize() // sätter count till 0 , anger också det slumpade talets värde
         {
-          
+            _count = 0;
+            Random random = new Random();
+            _number = random.Next(0, 101);
         }
-        public bool MakeGuess(int number)
+        public bool MakeGuess(int number) // Här utförs vad som händer när användaren matar in olika värden.
         {
+            if (_count >= MaxNumberOffGuesses) // om du gissar efter att gissningarna är slut, körs ett undantag
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _count++;
+            if (number == _number) // Om svaret är korrekt presenteras det med hur många försök det tog
+            {
+                Console.WriteLine("Korrekt! det tog {0} försök att få fram talet {1}",_count,_number);
+            }
+            if (_count == MaxNumberOffGuesses) // vad som visas när du har slut på gissningar
+                Console.WriteLine("Du har slut på gissningar, Svaret var {0}",_number);
+            if ( number < 1 || number > 100) // om du gissar lägre eller högre än vad som är max/min körs ett undantag.
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number > _number) // om du gissar för högt visas kod som säger detta + hur många försök som är kvar.
+            {
+                Console.WriteLine("{0} är ett för stort tal, gissa lägre.",number, (MaxNumberOffGuesses - _count));
+                return false;
+            }
+            if (number < _number) // om du gissar för lågt visas det + hur många försök som återstår.
+            {
+                Console.WriteLine("{0} är ett för litet tal, gissa högre.", number, (MaxNumberOffGuesses - _count));
+                return false;
+            }
 
         }
     }
