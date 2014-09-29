222a178460580b6284432ec4caa9b74aee067b59
MartinArvidsson <ma223ku@lnu.student.se>
2014-09-29 14:38:54 +0200

    påbörjat 2.1, förstår ingenting, gör webb idag istället för att få något gjort

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+        int _Number;
+        int _Count;
+            public void initialize(); 
+        {
+
+        }
+            public bool MakeGuess (int number);
+        {
+
+        }
+           public SecretNumber();
+        {
+
+        }
+    }
+}
